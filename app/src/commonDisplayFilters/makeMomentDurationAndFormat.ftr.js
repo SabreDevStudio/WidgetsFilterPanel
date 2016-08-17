adced78449c8dac03b5214f06ae283920101a3b8
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-08-17 17:04:35 +0200

    Patch corner case in moment duration humanize

--- app/src/commonDisplayFilters/makeMomentDurationAndFormat.ftr.js
+++ app/src/commonDisplayFilters/makeMomentDurationAndFormat.ftr.js
@@ -13,6 +13,10 @@ define([
             if (_.isUndefined(value) || value === null) {
                 return '';
             }
-            return moment.duration(value, momentFormat).humanize(suffix);
+            var duration = moment.duration(value, momentFormat);
+            if (duration.asMilliseconds() === 0) {
+                return 0; // patching moment duration corner case when for duration of 0 it humanizes as 'a few seconds'
+            }
+            return duration.humanize(suffix);
         };
     }    });
